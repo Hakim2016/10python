f6162898d48b892734f722a34c8b7c38c889edc6
hakim2016 <13270828661@163.com>
2017-10-04 13:26:37 +0800

    dorm

--- 171001dict163.py
+++ 171001dict163.py
@@ -20,12 +20,15 @@ import json
 # youdao_url = 'http://account.youdao.com/login?service=dict&back_url=http://dict.youdao.com/wordbook/wordlist'
 # youdao_url = 'http://account.youdao.com/login?service=dict&back_url=http%3A%2F%2Fdict.youdao.com%2Fsearch%3Fq%3Dhakim%26tab%3D%23keyfrom%3D%24%7Bkeyfrom%7D'
 yd_login_url = 'https://logindict.youdao.com/login/acc/login'
+# http://dict.youdao.com/wordbook/wordlist
 wrdbk_url = 'http://dict.youdao.com/wordbook/wordlist?keyfrom=login_from_dict2.index'
+# wrdbk_url = 'http://dict.youdao.com/wordbook/wordlist'
 # 构造 Request headers
 agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36'
 headers = {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
     'Host': 'dict.youdao.com',
+    'Upgrade-Insecure-Requests': '1',
     # 'Origin': 'http://account.youdao.com',
     # 'Referer': 'http://account.youdao.com/login?service=dict&back_url=http%3A%2F%2Fdict.youdao.com%2Fsearch%3Fq%3Dhakim%26tab%3D%23keyfrom%3D%24%7Bkeyfrom%7D',
     'User-Agent': agent
@@ -65,7 +68,11 @@ def login(email, secret):
     login = session.post(yd_login_url, headers=headers, data=postdata)
     session.cookies.save()
     cntnt = login.text
-    with open('./others/youdao.html', 'w', encoding='utf-8') as f:
+    output_html(cntnt, 'youdao.html')
+
+
+def output_html(cntnt, name):
+    with open('./others/' + name, 'w', encoding='utf-8') as f:
         f.write(cntnt)
 
 
@@ -77,11 +84,12 @@ def encrypt_psswd(psswd):
 
 
 def isLogin():
-    headers['Referer'] = 'http://dict.youdao.com/?keyfrom=dict2.top'
+    headers['Referer'] = 'http://www.youdao.com/'
     # browser private info to verify if we successfully login
-    login_code = session.get(wrdbk_url, headers=headers).status_code
-    print(login_code)
-    if login_code == 200:
+    sssn_rsp = session.get(wrdbk_url, headers=headers)
+    output_html(sssn_rsp.text, 'youdao2.html')
+    # print(sssn_rsp.text)
+    if sssn_rsp.status_code == 200:
         return True
     else:
         return False
@@ -91,6 +99,7 @@ if __name__ == '__main__':
     if isLogin():
         print('You have already login!')
     else:
+        print('prepare to login!')
         # email = input('please input your account!\n')
         # psswd = input('please input your password!\n')
         email = '13270828661@163.com'
