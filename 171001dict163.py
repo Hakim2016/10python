4aa438395ac0e7e077087a1817b7ea40f54896b6
hakim2016 <13270828661@163.com>
2017-10-04 12:04:01 +0800

    dict163

--- 171001dict163.py
+++ 171001dict163.py
@@ -13,18 +13,21 @@ import requests
 import re
 import http.cookiejar as cookielib
 import hashlib
+import os
 import json
 
 # youdao_url = 'http://account.youdao.com/login'
 # youdao_url = 'http://account.youdao.com/login?service=dict&back_url=http://dict.youdao.com/wordbook/wordlist'
-# youdao_url = 'http://account.youdao.com/login?service=dict&back_url=http%3A%2F%2Fdict.youdao.com%2Fsearch%3Fq%3Dresume%26tab%3D%23keyfrom%3D%24%7Bkeyfrom%7D'
-youdao_url = 'https://logindict.youdao.com/login/acc/login'
+# youdao_url = 'http://account.youdao.com/login?service=dict&back_url=http%3A%2F%2Fdict.youdao.com%2Fsearch%3Fq%3Dhakim%26tab%3D%23keyfrom%3D%24%7Bkeyfrom%7D'
+yd_login_url = 'https://logindict.youdao.com/login/acc/login'
+wrdbk_url = 'http://dict.youdao.com/wordbook/wordlist?keyfrom=login_from_dict2.index'
 # 构造 Request headers
 agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36'
 headers = {
-    'Host': 'shared.ydstatic.com',
-    'Origin': 'http://account.youdao.com',
-    'Referer': 'http://account.youdao.com/login?service=dict&back_url=http%3A%2F%2Fdict.youdao.com%2Fsearch%3Fq%3Dhakim%26tab%3D%23keyfrom%3D%24%7Bkeyfrom%7D',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Host': 'dict.youdao.com',
+    # 'Origin': 'http://account.youdao.com',
+    # 'Referer': 'http://account.youdao.com/login?service=dict&back_url=http%3A%2F%2Fdict.youdao.com%2Fsearch%3Fq%3Dhakim%26tab%3D%23keyfrom%3D%24%7Bkeyfrom%7D',
     'User-Agent': agent
 }
 
@@ -39,12 +42,8 @@ except:
 
 
 def login(email, secret):
-    print('before encode email is ' + email)
-    email = email.encode('utf-8')
-    print('after encode email is ')
-    print(email)
     secret = encrypt_psswd(secret)
-    params = {
+    postdata = {
         'app': 'web',
         'tp': 'urstoken',
         'cf': 3,
@@ -53,19 +52,22 @@ def login(email, secret):
         'product': 'DICT',
         'type': 1,
         'um': 'true',
-        'username': '13270828661@163.com',
+        'username': email,
         'password': secret,
     }
-    postdata = {
-        'username': '13270828661@163.com',
-        'password': secret,
+
+    login_headers = {
+        'Host': 'logindict.youdao.com',
+        'Origin': 'http://account.youdao.com',
+        'Referer': 'http://account.youdao.com/login?service=dict&back_url=http%3A%2F%2Fdict.youdao.com%2Fsearch%3Fq%3Dhakim%26tab%3D%23keyfrom%3D%24%7Bkeyfrom%7D',
+        'User-Agent': agent
     }
-    # postdata = json.dumps(postdata)
-    # login = session.post(youdao_url, data=postdata, headers=headers, params=params)
-    login = session.post(youdao_url, headers=headers, params=params)
+    login = session.post(yd_login_url, headers=headers, data=postdata)
     session.cookies.save()
     cntnt = login.text
-    print(cntnt)
+    with open('./others/youdao.html', 'w', encoding='utf-8') as f:
+        f.write(cntnt)
+
 
 def encrypt_psswd(psswd):
     psswd = psswd.encode('utf-8')
@@ -73,20 +75,24 @@ def encrypt_psswd(psswd):
     m.update(psswd)
     return m.hexdigest()
 
+
 def isLogin():
+    headers['Referer'] = 'http://dict.youdao.com/?keyfrom=dict2.top'
     # browser private info to verify if we successfully login
-    url = 'http://dict.youdao.com/wordbook/wordlist?keyfrom=login_from_dict2.index'
-    login_code = session.get(url, headers=headers, allow_redirects=False).status_code
+    login_code = session.get(wrdbk_url, headers=headers).status_code
     print(login_code)
     if login_code == 200:
         return True
     else:
         return False
 
+
 if __name__ == '__main__':
     if isLogin():
         print('You have already login!')
     else:
-        email = input('please input your account!\n')
-        psswd = input('please input your password!\n')
+        # email = input('please input your account!\n')
+        # psswd = input('please input your password!\n')
+        email = '13270828661@163.com'
+        psswd = '0513865210hjj'
         login(email, psswd)
--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # 10python
 
 Zhihu login!
+
+20171004
+    dict163 login
+    The request headers vary from different requests(like login requests and wordbook requests)
+    Need to construct different headers.
+
+        login_code = session.get(wrdbk_url, headers=headers, allow_redirects=False).status_code
+        不允许重定向 遇到重定向的网址 会报错
