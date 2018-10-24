45450d10cb1f38d39759c80af91952f662ebd0fe
何晶晶 / He，Jing Jing <jingjing.he@itg.hitachi.cn>
2018-10-24 11:18:13 +0800

    20181024

--- excel/start.py
+++ excel/start.py
@@ -1,6 +1,7 @@
 import xlwings as xw
 
-wb = xw.Book(r'C:\Users\Hakim\Desktop\Others\20180320 Header Edi.xlsx')
+# wb = xw.Book(r'C:\Users\Hakim\Desktop\Others\20180320 Header Edi.xlsx')
+wb = xw.Book(r'C:\Users\71346904\Desktop\Others\20181021\20181021 Ink data.xlsx')
 print(wb.name)
 sht = wb.sheets.active
 sht.range('B5').value = 'Hakim'
